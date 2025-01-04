f072e137f7d171d0926c9a63e7a4690f435b93bd
samsulmaarif1076 <samsulmaarif1076@gmail.com>
2025-01-04 06:32:21 +0000

    fix: ProductCategoryController

--- app/Http/Controllers/API/ProductCategoryController.php
+++ app/Http/Controllers/API/ProductCategoryController.php
@@ -13,7 +13,7 @@ class ProductCategoryController extends Controller
     public function all(Request $request)
     {
         $id = $request->input('id');
-        $limit = $request->input('limit$limit');
+        $limit = $request->input('limit');
         $name = $request->input('id');
         $show_product = $request->input('show_product');
 
